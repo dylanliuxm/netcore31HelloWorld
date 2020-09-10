c58f0ef222ecfd4fe4b3c2a0538b62c3ada8c01f
Sepehr Pakbaz <sepakbaz@microsoft.com>
2020-09-10 10:33:37 -0700

    added app insight

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -5,20 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+//using Microsoft.ApplicationInsights;
+//using Microsoft.ApplicationInsights.DataContracts
 namespace netcore.Pages
 {
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+        //private readonly TelemetryClient telemetry;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
+            //this.telemetry = telemetry;
         }
 
         public void OnGet()
         {
+            //throw new Exception("Testing application insights");
+            //this.telemetry.TrackEvent("PrivacyPageRequested");
+            //this.telemetry.TrackTrace("Critical Trace", SeverityLevel.Critical);
+
         }
     }
 }
